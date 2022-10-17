4b9efa2db787e386cf1a1dde2695c3f8ea3be19b
Howstheaqua <hughes.e.47201@gmail.com>
2022-10-16 23:59:39 -0400

    organizing

---
+++ itis3135/scripts/arrays.js
@@ -0,0 +1,14 @@
+let people = ['evan hughes', 'hannah grubb', 'jacob arrowood', 'joe mama', 'hugh mungus', 'i.c. weiner', 'seymore asses'];
+let salaries = [1000000.99, 0.99, 420000.99, 520000.54, 69000.58, 89000.99, 129000.99];
+
+function addSalary() {
+
+}
+
+function displayResults() {
+
+}
+
+function displaySalary() {
+
+}
