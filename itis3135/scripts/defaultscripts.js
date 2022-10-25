004c6a5e22d198fa4172b748725a42b71084eebc
Howstheaqua <hughes.e.47201@gmail.com>
2022-10-24 20:55:52 -0400

    large changes

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -3,14 +3,14 @@ var quillCount = 0;
 function scriptTest(){
     alert('This is a test!');
 }
-function submitGreeting(){
+function promptUser(){
     var name = document.getElementById('name').value;
     var mood = document.getElementById('mood').value;
     var greeting = "The " + companyName + " welcomes you, " + name + "! We're glad you are doing " + mood;
     document.getElementById('greeting').innerHTML = greeting;
 }
 
-function getDateString(){
+function getTodaysDate(){
     var todaysDate = new Date();
     var dateString = 'Today is ' + todaysDate.getHours() + ':' + todaysDate.getMinutes() + ' on ' + todaysDate.toLocaleString('default', {weekday: 'long'}) + ', ' + todaysDate.getDate() + ' ' + todaysDate.toLocaleString('default', {month: 'long'}) + ', ' + todaysDate.getFullYear();
     return dateString;
@@ -33,4 +33,4 @@ function perimeterTask(){
         width + ' and length ' + length + ' is ' + perimeter;
 }
 
-document.getElementById('date').innerHTML = getDateString();
+document.getElementById('date').innerHTML = getTodaysDate();
