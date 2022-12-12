9c399065ecb18cd0c92fc03f5cc049b10c3fca21
Howstheaqua <hughes.e.47201@gmail.com>
2022-12-11 23:30:20 -0500

    accessibility and activity 8

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -1,14 +1,59 @@
-let people = ['evan hughes', 'hannah grubb', 'jacob arrowood', 'joe mama', 'hugh mungus', 'i.c. weiner', 'seymore asses'];
-let salaries = [1000000.99, 0.99, 420000.99, 520000.54, 69000.58, 89000.99, 129000.99];
+let person = [];
+let salaries = [];
 
-function addSalary() {
-
+window.onload = function ()
+{
+    document.getElementById("addSalary").onclick = addSalary;
+    document.getElementById("displaySalary").onclick = displaySalary;
+    document.getElementById("displayResults").onclick = displayResults;
+    setFocusOnEmployee();
 }
 
-function displayResults() {
-
+function displayResults(){
+    // get average of salaries
+    let total = 0;
+    for(let i = 0; i < salaries.length; i++){
+        total += salaries[i];
+    }
+    let average = total / salaries.length;
+    // get highest salary
+    let highest = 0;
+    for(let i = 0; i < salaries.length; i++){
+        if(salaries[i] > highest){
+            highest = salaries[i];
+        }
+    }
+    resultsDiv = document.getElementById("results");
+    resultsDiv.innerHTML = '<h2>Results</h2><p>Average Salary: ' + average + '</p><p>Highest Salary: ' + highest + '</p>';
+}
+function addSalary(){
+    let select = document.getElementById("employee");
+    let value = select.options[select.selectedIndex].value;
+    if(value === "default"){
+        alert("Please select an employee");
+        setFocusOnEmployee();
+        return;
+    }
+    let salary = parseFloat(document.getElementById("salary").value);
+    if(isNaN(salary)){
+        alert("Salary must be a number");
+        document.getElementById("salary").focus();
+        return;
+    }
+    else{
+        person.push(value);
+        salaries.push(salary);
+        setFocusOnEmployee();
+    }
 }
 
-function displaySalary() {
-
+function displaySalary(){
+    table = document.getElementById('results-table');
+    table.innerHTML = '<tr><th>Name</th><th>Salary</th></tr>';
+    for(let i = 0; i < person.length; i++){
+        table.innerHTML += '<tr><td>' + person[i] + '</td><td>' + salaries[i] + '</td></tr>';
+    }
+}
+function setFocusOnEmployee(){
+    document.getElementById("employee").focus();
 }
